ca4c8df90f057acc3e149736a780e25c037cfdf1
Andy Armstrong <andy@hexten.net>
2008-09-19 11:02:47 +0000

    Snapshot

---
+++ andy/make-patch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+orig=$1
+dir=$2
+patches=../patches
+
+name=`basename $dir`
+patch=$patches/$name.patch
+echo Extracting patches for $name
+diff -ru $orig $dir | egrep -v '^Only in ' > $patch
+[ -s $patch ] || rm -f $patch
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
